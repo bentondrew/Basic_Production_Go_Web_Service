5803b53781f991675d93e5cd0c8058937f77b3f1
Benton Drew <benton.s.drew@drewantech.com>
2018-09-06 18:27:40 -0500

    Put header modification in the correct place.

--- source/service.go
+++ source/service.go
@@ -8,8 +8,9 @@ var version string = "0.0.3"
 
 func main() {
   mux := http.NewServeMux()
-  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-  w.WriteHeader(http.StatusOK)
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){w.Write([]byte("Hello!"))})
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("Hello!"))})
   http.ListenAndServe(":8080", mux)
 }
