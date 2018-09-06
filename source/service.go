52b9a9397f3a7a3a82fe0256fce05bc8d640158a
Benton Drew <benton.s.drew@drewantech.com>
2018-09-06 18:18:57 -0500

    Change to basic http server.

--- source/service.go
+++ source/service.go
@@ -2,10 +2,13 @@ package main
 
 import (
   "fmt"
+  "net/http"
 )
 
-var version string = "0.0.1"
+var version string = "0.0.2"
 
 func main() {
-  fmt.Println("Hello!")
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){w.Write("Hello!")})
+  http.ListenAndServe(":8080", mux)
 }
