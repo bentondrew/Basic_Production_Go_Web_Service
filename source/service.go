7a0b71f5707063d363b09fe9fb1b7be7f847a0e7
Benton Drew <benton.s.drew@drewantech.com>
2018-09-13 18:49:37 -0500

    Log error from ListenAndServe

--- source/service.go
+++ source/service.go
@@ -2,12 +2,13 @@ package main
 
 import (
   "net/http"
+  "log"
 )
 
 // Following video: https://www.youtube.com/watch?v=bM6N-vgPlyQ&t=2059s
 // Left off at: 4:55
 
-var version string = "0.0.3"
+var version string = "0.0.4"
 
 func main() {
   mux := http.NewServeMux()
@@ -15,5 +16,8 @@ func main() {
     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
     w.WriteHeader(http.StatusOK)
     w.Write([]byte("Hello!"))})
-  http.ListenAndServe(":8080", mux)
+  err := http.ListenAndServe(":8080", mux)
+  if err != nil {
+    log.Fatalf("Server failed to start: %v", err)
+  }
 }
