f14a5e0aa2ac160d4f8e4a9b11505af81915992e
Benton Drew <benton.s.drew@drewantech.com>
2018-09-17 20:39:01 -0500

    Move home handler function to own package and use a common logger for whole service.

---
+++ source/home/home.go
@@ -0,0 +1,26 @@
+package home
+
+import {
+  "net/http"
+  "log"
+}
+
+
+type Handlers struct {
+  logger *log.Logger
+}
+
+
+func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
+  h.logger.Println("Home request processed.")
+  w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("Hello!"))
+}
+
+
+func NewHandlers(logger *log.Logger) *Handlers {
+  return &Handlers{
+    logger: logger,
+  }
+}
--- source/main.go
+++ source/main.go
@@ -5,13 +5,16 @@ import (
   "log"
   "os"
   "service/server"
+  "service/home"
 )
 
 // Following video: https://www.youtube.com/watch?v=bM6N-vgPlyQ&t=2059s
-// Left off at: 6:55
+// Left off at: 13.24
 // Repo for tutorial: https://github.com/dlsniper/gopherconuk
 
-var version string = "0.0.6"
+
+var version string = "0.0.7"
+
 
 var (
   certFile = os.Getenv("CERT_FILE")
@@ -19,15 +22,16 @@ var (
   serviceAddr = os.Getenv("SERVICE_ADDR")
 )
 
+
 func main() {
+  logger := log.New(os.Stdout, "Basic Web Service", log.LstdFlags|log.Lshortfile)
+  h := home.NewHandlers(logger)
   mux := http.NewServeMux()
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("Hello!"))})
+  mux.HandleFunc("/", h.Home())
   srv := server.New(mux, serviceAddr)
+  logger.Println("Server starting.")
   err := srv.ListenAndServeTLS(certFile, keyFile)
   if err != nil {
-    log.Fatalf("Server failed to start: %v", err)
+    logger.Fatalf("Server failed to start: %v", err)
   }
 }
